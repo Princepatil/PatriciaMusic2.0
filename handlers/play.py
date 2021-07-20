9c4a92d8f6f4ffd0530df3ff3274eb579b45bd5a
tana9373 <83992274+tana9373@users.noreply.github.com>
2021-07-20 20:36:49 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -143,7 +143,7 @@ async def play(_, message: Message):
 
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/caeb50039026a746e7252.jpg"
+        thumb_name = "https://telegra.ph//file/d078b67f72dcddf6d09c7.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
@@ -153,7 +153,7 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton(
                         text="Channel 🔊",
-                        url="https://t.me/patricia_updates")
+                        url="https://t.me/its_tanya_update")
                    
                 ]
             ]
@@ -201,7 +201,7 @@ async def play(_, message: Message):
             )
         except Exception as e:
             title = "NaN"
-            thumb_name = "https://telegra.ph/file/86df5e0db1f70a3f5580f.png"
+            thumb_name = "https://telegra.ph//file/d078b67f72dcddf6d09c7.jpg"
             duration = "NaN"
             views = "NaN"
             keyboard = InlineKeyboardMarkup(
